a814ea8bcc91cb6f3d6e347b74d960cd1c965aed
rdzhyhar <dgigarroma@gmail.com>
2021-12-25 16:47:10 +0200

    .

---
+++ EasyRestTests/EasyRestTests/BaseTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace Tests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://localhost:3000/");
+
+        }
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
