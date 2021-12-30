26d0e560fa30d09fc317b7ca3a88ce5137aa0509
rdzhyhar <dgigarroma@gmail.com>
2021-12-30 16:05:47 +0200

    Add ModeratorRestaurantsManagmentAllRestaurantsListPageObject Add collections nugget Update nunit nugete

--- EasyRestTests/EasyRestTests/ModeratorTest.cs
+++ EasyRestTests/EasyRestTests/ModeratorTest.cs
@@ -19,10 +19,12 @@ namespace Tests
             signInPageObject.ClickSubmitButton();
             LoginedUserPartOfBaseHeaderPageObject loginedUserPartOfBaseHeaderPageObject = new LoginedUserPartOfBaseHeaderPageObject(driver);
             Assert.IsTrue(loginedUserPartOfBaseHeaderPageObject.UserMenuDisplayed());
+            //PositiveSignInTest
             loginedUserPartOfBaseHeaderPageObject.ClickUserMenuButton();
             string actualRolePanelText = loginedUserPartOfBaseHeaderPageObject.GetRolePanelText();
             string expectedRolePanelText = "Moderator panel";
             Assert.AreEqual(expectedRolePanelText, actualRolePanelText, $"{expectedRolePanelText} is not equal for {actualRolePanelText}");
+            //PositiveModeratorSignInTest
 
         }
         [Test]
@@ -45,9 +47,32 @@ namespace Tests
             loginedUserPartOfBaseHeaderPageObject.ClickRolePanelButton();
             ModeratorRestaurantsManagmentPageObject moderatorRestaurantsManagmentPageObject = new ModeratorRestaurantsManagmentPageObject(driver);
             moderatorRestaurantsManagmentPageObject.ClickRestaurantsManagmentPageButton();
-
+            //PositiveModeratorRestaurantsManagmentTest
         }
-
+        [Test]
+        public void PositiveModeratorRestaurantsManagmentAllRestaurantsListTest()
+        {
+            UnloginedUserPartOfBaseHeaderPageObject unloginedUserPartOfBaseHeader = new UnloginedUserPartOfBaseHeaderPageObject(driver);
+            unloginedUserPartOfBaseHeader.ClickSignInButton();
+            SignInPageObject signInPageObject = new SignInPageObject(driver);
+            signInPageObject.SendTextToEmailTextField("petermoderator@test.com");
+            signInPageObject.SendTextToPasswordTextField("1");
+            signInPageObject.ClickSubmitButton();
+            LoginedUserPartOfBaseHeaderPageObject loginedUserPartOfBaseHeaderPageObject = new LoginedUserPartOfBaseHeaderPageObject(driver);
+            Assert.IsTrue(loginedUserPartOfBaseHeaderPageObject.UserMenuDisplayed());
+            //PositiveSignInTest
+            loginedUserPartOfBaseHeaderPageObject.ClickUserMenuButton();
+            string actualRolePanelText = loginedUserPartOfBaseHeaderPageObject.GetRolePanelText();
+            string expectedRolePanelText = "Moderator panel";
+            Assert.AreEqual(expectedRolePanelText, actualRolePanelText, $"{expectedRolePanelText} is not equal for {actualRolePanelText}");
+            //PositiveModeratorSignInTest
+            loginedUserPartOfBaseHeaderPageObject.ClickRolePanelButton();
+            ModeratorRestaurantsManagmentPageObject moderatorRestaurantsManagmentPageObject = new ModeratorRestaurantsManagmentPageObject(driver);
+            moderatorRestaurantsManagmentPageObject.ClickRestaurantsManagmentPageButton();
+            //PositiveModeratorRestaurantsManagmentTest
+            
+            
+        }
 
 
 
---
+++ EasyRestTests/PageObjects/ModeratorRestaurantsManagmentAllRestaurantsListPageObject.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PageObjects
+{
+    public class ModeratorRestaurantsManagmentAllRestaurantsListPageObject : ModeratorRestaurantsManagmentPageObject
+    {
+        public ModeratorRestaurantsManagmentAllRestaurantsListPageObject(IWebDriver driver) : base(driver)
+        {
+
+        }
+        List<IWebElement> allRestaurants = driver.FindElement(By.CssSelector(".MuiGrid-container-4193.MuiGrid-spacing-xs-16-4216")).FindElements(By.TagName("div")).ToList();
+        
+
+    }
+}
