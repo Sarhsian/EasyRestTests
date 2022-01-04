19ef51102efeb00e7b0aca60fefbc54a1db71fdc
rdzhyhar <dgigarroma@gmail.com>
2022-01-04 15:41:48 +0200

    PositiveModeratorRestaurantsManagmentUnapprovedRestaurantsListTest

--- EasyRestTests/EasyRestTests/ModeratorTest.cs
+++ EasyRestTests/EasyRestTests/ModeratorTest.cs
@@ -73,7 +73,8 @@ namespace Tests
             ModeratorRestaurantsManagmentPageObject moderatorRestaurantsManagmentPageObject 
                 = new ModeratorRestaurantsManagmentPageObject(driver);
             moderatorRestaurantsManagmentPageObject.ClickRestaurantsManagmentPageButton();
-            //PositiveModeratorRestaurantsManagmentTest           
+            //PositiveModeratorRestaurantsManagmentTest
+            moderatorRestaurantsManagmentPageObject.ClickAllRestaurantsListButton();
             ModeratorRestaurantsManagmentAllRestaurantsListPageObject moderatorRestaurantsManagmentAllRestaurantsListPageObject
                 = new ModeratorRestaurantsManagmentAllRestaurantsListPageObject(driver);
             string expectedDisapprovedMessageText = "Disapproved";
@@ -94,9 +95,45 @@ namespace Tests
             actualApprovedMessageText = moderatorRestaurantsManagmentAllRestaurantsListPageObject.GetApprovedMessageText();
             Assert.AreEqual(expectedApprovedMessageText, actualApprovedMessageText, $"{expectedApprovedMessageText} is not equal for {actualApprovedMessageText}");
 
+            //PositiveModeratorRestaurantsManagmentAllRestaurantsListTest
 
+        }
+        [Test]
+        public void PositiveModeratorRestaurantsManagmentUnapprovedRestaurantsListTest()
+        {
+            UnloginedUserPartOfBaseHeaderPageObject unloginedUserPartOfBaseHeader = new UnloginedUserPartOfBaseHeaderPageObject(driver);
+            unloginedUserPartOfBaseHeader.ClickSignInButton();
+            SignInPageObject signInPageObject = new SignInPageObject(driver);
+            signInPageObject.SendTextToEmailTextField("petermoderator@test.com");
+            signInPageObject.SendTextToPasswordTextField("1");
+            signInPageObject.ClickSubmitButton();
+            LoginedUserPartOfBaseHeaderPageObject loginedUserPartOfBaseHeaderPageObject = new LoginedUserPartOfBaseHeaderPageObject(driver);
+            Assert.IsTrue(loginedUserPartOfBaseHeaderPageObject.UserMenuDisplayed());
+            //PositiveSignInTest
+            loginedUserPartOfBaseHeaderPageObject.ClickUserMenuButton();
+            string actualRolePanelText = loginedUserPartOfBaseHeaderPageObject.GetRolePanelText();
+            string expectedRolePanelText = "Moderator panel";
+            Assert.AreEqual(expectedRolePanelText, actualRolePanelText, $"{expectedRolePanelText} is not equal for {actualRolePanelText}");
+            //PositiveModeratorSignInTest
+            loginedUserPartOfBaseHeaderPageObject.ClickRolePanelButton();
+            ModeratorRestaurantsManagmentPageObject moderatorRestaurantsManagmentPageObject
+                = new ModeratorRestaurantsManagmentPageObject(driver);
+            moderatorRestaurantsManagmentPageObject.ClickRestaurantsManagmentPageButton();
+            //PositiveModeratorRestaurantsManagmentTest
+            moderatorRestaurantsManagmentPageObject.ClickUnapprovedRestaurantsListButton();
+            ModeratorRestaurantsManagmentAllRestaurantsListPageObject moderatorRestaurantsManagmentAllRestaurantsListPageObject
+                = new ModeratorRestaurantsManagmentAllRestaurantsListPageObject(driver);
+            string expectedDisapprovedMessageText = "Disapproved";
+            string expectedApprovedMessageText = "Approved";
+            moderatorRestaurantsManagmentAllRestaurantsListPageObject.ClickRandomRestaurantsButtonDisapprove();
+            string actualDisapprovedMessageText = moderatorRestaurantsManagmentAllRestaurantsListPageObject.GetDisapprovedMessageText();
+            Assert.AreEqual(expectedDisapprovedMessageText, actualDisapprovedMessageText, $"{expectedDisapprovedMessageText} is not equal for {actualDisapprovedMessageText}");
 
+            moderatorRestaurantsManagmentAllRestaurantsListPageObject.ClickRandomRestaurantsButtonApprove();
+            string actualApprovedMessageText = moderatorRestaurantsManagmentAllRestaurantsListPageObject.GetApprovedMessageText();
+            Assert.AreEqual(expectedApprovedMessageText, actualApprovedMessageText, $"{expectedApprovedMessageText} is not equal for {actualApprovedMessageText}");
 
+            //PositiveModeratorRestaurantsManagmentUnapprovedRestaurantsListTest
 
         }
 
