f1876ceb72d4bf01afc7c60e03f7f38b085cad3d
Wallert <stovpovecz@ukr.net>
2022-01-05 11:25:06 +0200

    Added AdminUsersPage and 1 test.

---
+++ EasyRestTests/EasyRestTests/UsersFromAdminTest.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using PageObjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    public class UsersFromAdminTest : BaseTest
+    {
+        [Test]
+        public void BanUsersFromAllTab()
+        {
+            // Arrange            
+            UnloginedUserPartOfBaseHeaderPageObject unloginedUserPartOfBaseHeader = new UnloginedUserPartOfBaseHeaderPageObject(driver);
+            SignInPageObject signInPageObject = new SignInPageObject(driver);
+            AdminUsersPage adminUsersPage = new AdminUsersPage(driver);
+            string expectedText = "Active";
+            int countOfUsers = 10;
+            int actualUser = 1;
+
+            // Act            
+            unloginedUserPartOfBaseHeader.ClickSignInButton();
+            signInPageObject.SendTextToEmailTextField("steveadmin@test.com");
+            signInPageObject.SendTextToPasswordTextField("1");
+            signInPageObject.ClickSubmitButton();
+            for (; actualUser <= countOfUsers; actualUser++)
+            {
+                string actualText = adminUsersPage.GetActualStatus(actualUser);
+                if (string.Equals(expectedText, actualText))
+                {
+                    adminUsersPage.ClickLockButton(actualUser);
+                    break;
+                }
+            }
+            driver.Navigate().Refresh();
+            string actualText2 = adminUsersPage.GetActualStatus(actualUser);
+
+            //Assert
+            Assert.AreNotEqual(actualText2, expectedText, $"{actualText2} is not equal for {expectedText}");
+        }
+    }
+}
---
+++ EasyRestTests/PageObjects/AdminUsersPage.cs
@@ -0,0 +1,31 @@
+﻿using OpenQA.Selenium;
+
+
+namespace PageObjects
+{
+    public class AdminUsersPage
+    {
+        protected static IWebDriver driver;
+        public AdminUsersPage(IWebDriver webDriver)
+        {
+            driver = webDriver;
+        }
+        private IWebElement UsersButton => driver.FindElement(By.XPath("//span[text()='Users']"));
+        private IWebElement OwnersButton => driver.FindElement(By.XPath("//span[text()='Owners']"));
+        private IWebElement ModeratorsButton => driver.FindElement(By.XPath("//span[text()='Moderators']"));
+        private IWebElement RestaurantsButton => driver.FindElement(By.XPath("//span[text()='Restaurants']"));
+        private IWebElement AllUsersButton => driver.FindElement(By.XPath("//span[starts-with(text(),'All')]"));
+        private IWebElement ActiveUsersButton => driver.FindElement(By.XPath("//span[starts-with(text(),'Active')]"));
+        private IWebElement BannedUsersButton => driver.FindElement(By.XPath("//span[starts-with(text(),'Banned')]"));
+        private IWebElement LockButton(int numOfLock) => driver.FindElement(By.XPath("//table//tbody//tr[" + numOfLock + "]//td//button"));
+        private IWebElement ActualStatus(int numOfStatus) => driver.FindElement(By.XPath("//table//tbody//tr[" + numOfStatus + "]//td//p"));       
+        public void ClickLockButton(int numOfLock)
+        {
+            LockButton(numOfLock).Click();
+        }
+        public string GetActualStatus(int numOfStatus)
+        {
+            return ActualStatus(numOfStatus).Text;
+        }        
+    }
+}
