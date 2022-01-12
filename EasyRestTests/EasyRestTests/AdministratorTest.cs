52fc9fbd168e5ff4037a7fa29f990f4e2b90e233
Wallert <stovpovecz@ukr.net>
2022-01-12 13:55:12 +0200

    Added AdministratorTest

---
+++ EasyRestTests/EasyRestTests/AdministratorTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using PageObjects;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    class AdministratorTest : BaseTest
+    {
+        [Test]
+        public void AssignedWaiter()
+        {
+            //Arrange
+            UnloginedUserPartOfBaseHeaderPageObject unloginedUserPartOfBaseHeader = new UnloginedUserPartOfBaseHeaderPageObject(driver);
+            SignInPageObject signInPageObject = new SignInPageObject(driver);
+            AdministratorPanelPageObject administratorPanel = new AdministratorPanelPageObject(driver);
+            string exceptedText = "Order summary";
+            string actualText;
+
+            //Act
+            unloginedUserPartOfBaseHeader.ClickSignInButton();
+            signInPageObject.SendTextToEmailTextField("tanyasanchez@test.com");
+            signInPageObject.SendTextToPasswordTextField("1");
+            signInPageObject.ClickSubmitButton();
+            administratorPanel.ClickAssignedWaiterButton();
+            administratorPanel.ClickArrowDownButton(1);           
+            actualText = administratorPanel.GetOrderInfo();
+            //Assert
+            Assert.AreEqual(exceptedText, actualText, $"{actualText} is not equal {exceptedText} ");
+        }
+    }
+}
