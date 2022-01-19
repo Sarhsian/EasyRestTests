0929c786c8553dce91ab257e5b463460c6348990
w1nyx <vad.com.z@gmail.com>
2022-01-19 14:37:55 +0200

    Change variable

--- EasyRestTests/EasyRestTests/WaiterOrdersTest.cs
+++ EasyRestTests/EasyRestTests/WaiterOrdersTest.cs
@@ -9,16 +9,16 @@ namespace Tests
 		public void StartOrder()
 		{
 			//Arrange
-			var signInPageObject = new SignInPage(driver);
-			var waiterOrdersPageObject = new WaiterOrdersPage(driver);
+			var signInPage = new SignInPage(driver);
+			var waiterOrdersPage = new WaiterOrdersPage(driver);
 			string expectedString = "success";
 
 			//Act
-			signInPageObject.SignInAsWaiter2();
-			waiterOrdersPageObject.ClickAssignedWaiterTab();
-			waiterOrdersPageObject.ClickShowMoreButton();
-			waiterOrdersPageObject.ClickStartOrderButton();
-			string actualString = waiterOrdersPageObject.GetSuccessOrderMessage();
+			signInPage.SignInAsWaiter2();
+			waiterOrdersPage.ClickAssignedWaiterTab();
+			waiterOrdersPage.ClickShowMoreButton();
+			waiterOrdersPage.ClickStartOrderButton();
+			string actualString = waiterOrdersPage.GetSuccessOrderMessage();
 
 			// Assert
 			Assert.AreEqual(expectedString, actualString, $"{expectedString} is not equal {actualString}");
@@ -28,16 +28,16 @@ namespace Tests
 		public void CloseOrder()
 		{
 			//Arrange
-			var signInPageObject = new SignInPage(driver);
-			var waiterOrdersPageObject = new WaiterOrdersPage(driver);
+			var signInPage = new SignInPage(driver);
+			var waiterOrdersPage = new WaiterOrdersPage(driver);
 			string expectedString = "success";
 
 			//Act
-			signInPageObject.SignInAsWaiter2();
-			waiterOrdersPageObject.ClickInProgressTab();
-			waiterOrdersPageObject.ClickShowMoreButton();
-			waiterOrdersPageObject.ClickCloseOrderButton();
-			string actualString = waiterOrdersPageObject.GetSuccessOrderMessage();
+			signInPage.SignInAsWaiter2();
+			waiterOrdersPage.ClickInProgressTab();
+			waiterOrdersPage.ClickShowMoreButton();
+			waiterOrdersPage.ClickCloseOrderButton();
+			string actualString = waiterOrdersPage.GetSuccessOrderMessage();
 
 			// Assert
 			Assert.AreEqual(expectedString, actualString, $"{expectedString} is not equal {actualString}");
@@ -47,21 +47,21 @@ namespace Tests
 		public void GetAllOrderStatus()
 		{
 			//Arrange
-			var signInPageObject = new SignInPage(driver);
-			var waiterOrdersPageObject = new WaiterOrdersPage(driver);
+			var signInPage = new SignInPage(driver);
+			var waiterOrdersPage = new WaiterOrdersPage(driver);
 			int allOrdersCount;
 			int actualOrdersCount = 0;
 
 			//Act
-			signInPageObject.SignInAsWaiter2();
-			waiterOrdersPageObject.ClickAllTab();
-			allOrdersCount = waiterOrdersPageObject.GetOrdersTabCount();
-			waiterOrdersPageObject.ClickAssignedWaiterTab();
-			actualOrdersCount += waiterOrdersPageObject.GetOrdersTabCount();
-			waiterOrdersPageObject.ClickInProgressTab();
-			actualOrdersCount += waiterOrdersPageObject.GetOrdersTabCount();
-			waiterOrdersPageObject.ClickHistoryTab();
-			actualOrdersCount += waiterOrdersPageObject.GetOrdersTabCount();
+			signInPage.SignInAsWaiter2();
+			waiterOrdersPage.ClickAllTab();
+			allOrdersCount = waiterOrdersPage.GetOrdersTabCount();
+			waiterOrdersPage.ClickAssignedWaiterTab();
+			actualOrdersCount += waiterOrdersPage.GetOrdersTabCount();
+			waiterOrdersPage.ClickInProgressTab();
+			actualOrdersCount += waiterOrdersPage.GetOrdersTabCount();
+			waiterOrdersPage.ClickHistoryTab();
+			actualOrdersCount += waiterOrdersPage.GetOrdersTabCount();
 
 			// Assert
 			Assert.AreEqual(allOrdersCount, actualOrdersCount, $"{allOrdersCount} is not equal {actualOrdersCount}");
