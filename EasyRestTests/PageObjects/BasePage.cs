8dd5cab6e736b8e2cee055454dc64094235acfb2
sarhsian <sargsyan.mikhail.2017@gmail.com>
2022-01-20 12:17:47 +0200

    refactored BasePage

--- EasyRestTests/PageObjects/BasePage.cs
+++ EasyRestTests/PageObjects/BasePage.cs
@@ -1,19 +1,15 @@
 ﻿using OpenQA.Selenium;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace PageObjects
 {
-    public class BasePageObject
+    public class BasePage
     {
         protected IWebDriver driver;
 
-        public BasePageObject(IWebDriver webDriver)
+        public BasePage(IWebDriver webDriver)
         {
             driver = webDriver;
         }
-
 
         IWebElement PrevSlideButton => driver.FindElement(By.XPath("//button[@class='MuiButtonBase-root-106 MuiFab-root-1972 MuiFab-primary-1974 AppSlider-sliderBtnPrev-1966']"));
         IWebElement NextSlideButton => driver.FindElement(By.XPath("//button[@class='MuiButtonBase-root-106 MuiFab-root-1972 MuiFab-primary-1974 AppSlider-sliderBtnNext-1965']"));
