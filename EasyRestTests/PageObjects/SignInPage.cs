adcb48c3e2f907defa25fe236aad7353fd62bceb
sarhsian <sargsyan.mikhail.2017@gmail.com>
2022-01-20 12:23:48 +0200

    refactored methods SignInPage

--- EasyRestTests/PageObjects/SignInPage.cs
+++ EasyRestTests/PageObjects/SignInPage.cs
@@ -26,52 +26,22 @@ namespace PageObjects
             SendTextToPasswordTextField(password);
             ClickSubmitButton();
         }
-        public void SignInAsClient()
-        {
-            SignIn("katiedoyle@test.com", "1111");
-        }
-        public void SignInAsOwner()
-        {
-            SignIn("jasonbrown@test.com", "1111");
-        }
-        public void SignInAsModerator()
-        {
-            SignIn("petermoderator@test.com", "1");
-        }
-        public void SignInAsAdmin()
-        {
-            SignIn("steveadmin@test.com", "1");
-        }
-        public void SignInAsAdministrator()
-        {
-            SignIn("tanyasanchez@test.com", "1");
-        }
-        public void SignInAsWaiter1()
-        {
-            SignIn("karenperez@test.com", "1");
-        }
-        public void SignInAsWaiter2()
-        {
-            SignIn("heatherdalton@test.com", "1");
-        }
+        public void SignInAsClient() => SignIn("katiedoyle@test.com", "1111");
+        public void SignInAsOwner() => SignIn("jasonbrown@test.com", "1111");
+        public void SignInAsModerator() => SignIn("petermoderator@test.com", "1");
+        public void SignInAsAdmin() => SignIn("steveadmin@test.com", "1");
+        public void SignInAsAdministrator() => SignIn("tanyasanchez@test.com", "1");
+        public void SignInAsWaiter1() => SignIn("karenperez@test.com", "1");
+        public void SignInAsWaiter2() => SignIn("heatherdalton@test.com", "1");
         public void SendTextToEmailTextField(string text)
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
             wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//h5[text()='Sign In']")));
             EmailTextField.SendKeys(text);
         }
-        public void SendTextToPasswordTextField(string text)
-        {
-            PasswordTextField.SendKeys(text);
-        }
-        public void ClickBackButton()
-        {
-            BackButton.Click();
-        }
-        public void ClickSubmitButton()
-        {
-            SubmitButton.Click();
-        }
+        public void SendTextToPasswordTextField(string text) => PasswordTextField.SendKeys(text);
+        public void ClickBackButton() => BackButton.Click();
+        public void ClickSubmitButton() => SubmitButton.Click();
         public string GetEmailOrPasswordIsNotValidErrorMessage()
         {
             WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
