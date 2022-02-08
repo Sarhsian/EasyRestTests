53e236a40f0a5e1a9959047e19bd5a02366d82ea
w1nyx <vad.com.z@gmail.com>
2022-02-08 14:25:49 +0200

    Added api test with allure for waiter, and several api test with allure for moderator

--- EasyRestTests/EasyRestTests/ApiTests/ModeratorApiTest.cs
+++ EasyRestTests/EasyRestTests/ApiTests/ModeratorApiTest.cs
@@ -1,10 +1,192 @@
 ﻿using NUnit.Framework;
 using Leaf.xNet;
-using Newtonsoft.Json.Linq;
+using NUnit.Allure.Core;
+using NUnit.Allure.Attributes;
+using Allure.Commons;
 
 namespace Tests.ApiTests
 {
+    [AllureNUnit]
+    [AllureSuite("API")]
+    [AllureEpic("Moderator_API")]
+    [AllureLink("https://docs.google.com/spreadsheets/d/1KvQebEOdgZxL8gbtz1mG_5xvF9WzucCWdPmjLLTQuSw/edit#gid=1168516342")]
     class ModeratorApiTest : BaseApiTest 
     {
+        [Test, Order(1)]
+        [AllureDescription("Test for moderator role, to check posibility to approve restaurant")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00001")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Restaurants")]
+        [AllureStory("Approve")]
+        public void Restaurants_WhenLoggedInAsModerator_ShouldApproveRestaurant()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/restaurants";
+            string json = "{\"id\":10,\"status\":1}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Post(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test, Order(2)]
+        [AllureDescription("Test for moderator role, to check posibility to disapprove restaurant")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00001")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Restaurants")]
+        [AllureStory("Disapprove")]
+        public void Restaurants_WhenLoggedInAsModerator_ShouldDisapproveRestaurant()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/restaurants";
+            string json = "{\"id\":7,\"status\":2}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Delete(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test, Order(3)]
+        [AllureDescription("Test for moderator role, to check posibility to restore restaurant")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00003")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Restaurants")]
+        [AllureStory("Restore")]
+        public void Restaurants_WhenLoggedInAsModerator_ShouldRestoreRestaurant()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/restaurants";
+            string json = "{\"id\":9,\"status\":1}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Post(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test, Order(4)]
+        [AllureDescription("Test for moderator role, to check posibility to delete restaurant")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00002")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Restaurants")]
+        [AllureStory("Delete")]
+        public void Restaurants_WhenLoggedInAsModerator_ShouldDeleteRestaurant()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/restaurants";
+            string json = "{\"id\":8,\"status\":2}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Delete(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test, Order(5)]
+        [AllureDescription("Test for moderator role, to check posibility to ban users")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00005")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Users")]
+        [AllureStory("Ban")]
+        public void Users_WhenLoggedInAsModerator_ShouldBanUser()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/users";
+            string json = "{\"id\":18}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Post(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test, Order(6)]
+        [AllureDescription("Test for moderator role, to check posibility to unban users")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00005")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Users")]
+        [AllureStory("Unban")]
+        public void Users_WhenLoggedInAsModerator_ShouldUnbanUser()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/users";
+            string json = "{\"id\":18}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Post(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test, Order(7)]
+        [AllureDescription("Test for moderator role, to check posibility to ban owners")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00009")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Owners")]
+        [AllureStory("Ban")]
+        public void Owners_WhenLoggedInAsModerator_ShouldBanOwner()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/owners";
+            string json = "{\"id\":5}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Post(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test, Order(8)]
+        [AllureDescription("Test for moderator role, to check posibility to unban owners")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Moderator", "API", "TestCase ID#00010")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Owners")]
+        [AllureStory("Unban")]
+        public void Owners_WhenLoggedInAsModerator_ShouldUnbanOwner()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/moderator/owners";
+            string json = "{\"id\":5}";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsModerator(request));
+            request.Post(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
     }
 }
--- EasyRestTests/EasyRestTests/ApiTests/WaiterApiTest.cs
+++ EasyRestTests/EasyRestTests/ApiTests/WaiterApiTest.cs
@@ -1,12 +1,24 @@
 ﻿using NUnit.Framework;
 using Leaf.xNet;
-using Newtonsoft.Json.Linq;
+using NUnit.Allure.Core;
+using NUnit.Allure.Attributes;
+using Allure.Commons;
 
 namespace Tests.ApiTests
 {
+    [AllureNUnit]
+    [AllureSuite("API")]
+    [AllureEpic("Waiter_API")]
+    [AllureLink("https://docs.google.com/spreadsheets/d/1KvQebEOdgZxL8gbtz1mG_5xvF9WzucCWdPmjLLTQuSw/edit#gid=1420718935")]
     class WaiterApiTest : BaseApiTest
     {
         [Test]
+        [AllureDescription("Test for waiter role, to check posibility to start order")]
+        [AllureOwner("Vitalii")]
+        [AllureTag("Waiter", "API", "TestCase ID#00002")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Orders")]
+        [AllureStory("Start order")]
         public void AssignedWaiter_WhenLoggedInAsWaiter_ShouldStartOrder()
         {
             //Arrange
@@ -23,6 +35,12 @@ namespace Tests.ApiTests
             Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
         }
         [Test]
+        [AllureDescription("Test for waiter role, to check posibility to close order")]
+        [AllureOwner("Vitalii")]
+        [AllureTag("Waiter", "API", "TestCase ID#00001")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Orders")]
+        [AllureStory("Close order")]
         public void InProgress_WhenLoggedInAsWaiter_ShouldCloseOrder()
         {
             //Arrange
@@ -34,6 +52,27 @@ namespace Tests.ApiTests
             request.IgnoreProtocolErrors = true;
             request.AddHeader("x-auth-token", SignInToGetToken("karenperez@test.com", "1", request));
             request.Put(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test]
+        [AllureDescription("Test for waiter role, to check posibility to get info about orders")]
+        [AllureOwner("Vadym")]
+        [AllureTag("Waiter", "API", "TestCase ID#00003")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("Orders")]
+        [AllureStory("Info about orders")]
+        public void All_WhenLoggedInAsWaiter_ShouldGetInfo()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/waiter/orders";
+
+            //Act
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsWaiter1(request));
+            request.Get(url);
 
             //Assert
             Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
