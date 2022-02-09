0966eabc4b9d4a06b84e8c3ce9c2c9a74a00bca4
Wallert <stovpovecz@ukr.net>
2022-02-09 13:08:59 +0200

    Add 2 tests for unloginedUser

--- EasyRestTests/EasyRestTests/ApiTests/UnloginedApiTest.cs
+++ EasyRestTests/EasyRestTests/ApiTests/UnloginedApiTest.cs
@@ -1,12 +1,24 @@
 ﻿using NUnit.Framework;
 using Leaf.xNet;
-using Newtonsoft.Json.Linq;
+using NUnit.Allure.Core;
+using NUnit.Allure.Attributes;
+using Allure.Commons;
 
 namespace Tests.ApiTests
 {
+    [AllureNUnit]
+    [AllureEpic("UnloginedUser_API")]
+    [AllureSuite("API")]
+    [AllureLink("https://docs.google.com/spreadsheets/d/1KvQebEOdgZxL8gbtz1mG_5xvF9WzucCWdPmjLLTQuSw/edit#gid=1686602044")]
     class UnloginedApiTest : BaseApiTest
     {
         [Test]
+        [AllureDescription("Test to check possibility to sign in as client")]
+        [AllureOwner("Vitalii")]
+        [AllureTag("UnloginedUser", "API", "TestCase ID#00001")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("MainPage")]
+        [AllureStory("SignIn")]
         public void SignInAsClient()
         {
             //Arrange
@@ -22,6 +34,12 @@ namespace Tests.ApiTests
             Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
         }
         [Test]
+        [AllureDescription("Test to check possibility to sign up as client")]
+        [AllureOwner("Vitalii")]
+        [AllureTag("UnloginedUser", "API", "TestCase ID#00002")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("MainPage")]
+        [AllureStory("SignUp")]
         public void SignUpAsClient()
         {
             //Arrange
@@ -32,6 +50,47 @@ namespace Tests.ApiTests
             //Act            
             request.IgnoreProtocolErrors = true;
             request.Post(url, json, "json");
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test]
+        [AllureDescription("Test to check possibility to log out as client")]
+        [AllureOwner("Vitalii")]
+        [AllureTag("UnloginedUser", "API")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("MainPage")]
+        [AllureStory("LogOut")]
+        public void LogOutAsClient()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/login";            
+
+            //Act            
+            request.IgnoreProtocolErrors = true;
+            request.AddHeader("x-auth-token", SignInToGetTokenAsClient(request));
+            request.Delete(url);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
+        }
+        [Test]
+        [AllureDescription("Test to check possibility to check restaurants list")]
+        [AllureOwner("Vitalii")]
+        [AllureTag("UnloginedUser", "API")]
+        [AllureSeverity(SeverityLevel.normal)]
+        [AllureFeature("MainPage")]
+        [AllureStory("CheckRestaurants")]
+        public void CheckRestaurantList()
+        {
+            //Arrange
+            var request = new HttpRequest();
+            string url = "http://localhost:6543/api/restaurant";
+
+            //Act            
+            request.IgnoreProtocolErrors = true;            
+            request.Get(url);
 
             //Assert
             Assert.AreEqual(HttpStatusCode.OK, request.Response.StatusCode, $"OK is not equal for {request.Response.StatusCode}");
