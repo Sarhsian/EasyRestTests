5f3064e67115dc066f5fa1e14702dce4befc6af9
Wallert <stovpovecz@ukr.net>
2022-02-14 13:24:48 +0200

    Edit Base test

--- EasyRestTests/EasyRestTests/BaseTest.cs
+++ EasyRestTests/EasyRestTests/BaseTest.cs
@@ -2,6 +2,7 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System;
+using System.Diagnostics;
 
 namespace Tests
 {
@@ -20,6 +21,13 @@ namespace Tests
         public void TearDown()
         {
             driver.Quit();
+        }
+        public void ResetDB()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.FileName = "cmd";
+            psi.Arguments = @"/c cd /d D:\Work\Academy\FirstProj\master & initialize_easyrest_db --reset --fill development.ini";
+            Process.Start(psi);
         }
     }
 }
